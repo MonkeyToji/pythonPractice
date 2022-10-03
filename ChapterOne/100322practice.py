1485733d338cb0de74bf52ae1516023b2d4292a7
Matthew <mrdicke22@icloud.com>
2022-10-03 15:23:56 -0500

    ChapOne

---
+++ ChapterOne/100322practice.py
@@ -0,0 +1,11 @@
+#This program says hello and asks for my name!
+
+
+print("hello, world!")
+print("What is your name?") #asks for name
+myName = input()
+print('It is good to meet you, ' + myName)
+print(len(myName))
+print('What is your age?') #ask for the age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pythonPractice
 A place to practice my python coding.
+
+Automate the Boring Stuff with Python (source book)
