a638cc6e18f73ca7d7c106e542b21b04c22fbe1d
Matthew <mrdicke22@icloud.com>
2022-10-10 15:34:41 -0500

    chapter 4

---
+++ Chapter4/magic8ball.py
@@ -0,0 +1,5 @@
+import random
+
+messages = ['It is certain', 'it isdecidedly so', 'Reply hazy try again', 'Ask again later', 'concentrate and ask again', 'My reply is no', 'Outlook not so good', 'Very Doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
